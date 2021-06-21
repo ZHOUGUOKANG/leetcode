bec9a4c40dff21f182a7a786553d7d81bb151c70
zhouguokang <zhouguokang@tal.com>
2021-06-21 20:20:13 +0800

    +401

--- exercises/Num-401-readBinaryWatch/readBinaryWatch.go
+++ exercises/Num-401-readBinaryWatch/readBinaryWatch.go
@@ -1,5 +1,10 @@
 package Num_401_ReadBinaryWatch
 
+import (
+	"fmt"
+	"math/bits"
+)
+
 //1,2,4,8 0~11
 var hourMapping = map[int][]string{
 	0: {"0"},
@@ -14,7 +19,7 @@ var minMapping = map[int][]string{
 	1: {"01", "02", "04", "08", "16", "32"},
 	2: {"03", "05", "09", "17", "33", "06", "10", "18", "34", "12", "20", "36", "24", "40", "48"},
 	3: {"07", "11", "19", "35", "14", "22", "38", "28", "44", "56", "13", "21", "37", "25", "41", "49", "26", "42", "50", "52"},
-	4: {"15", "23", "39", "30", "46", "27", "43", "53", "29", "45", "46", "51", "54", "57", "58"},
+	4: {"15", "23", "39", "30", "46", "27", "43", "53", "29", "45", "51", "54", "57", "58"},
 	5: {"31", "47", "59", "55"},
 }
 
@@ -22,18 +27,25 @@ func readBinaryWatch(turnedOn int) (ans []string) {
 	if turnedOn >= 9 || turnedOn < 0 {
 		return
 	}
-	cache := map[string]struct{}{}
 	for i := 3; i >= 0; i-- {
 		if j := turnedOn - i; j >= 0 {
 			for _, h := range hourMapping[i] {
 				for _, m := range minMapping[j] {
-					cache[h+":"+m] = struct{}{}
+					ans = append(ans, h+":"+m)
 				}
 			}
 		}
 	}
-	for k, _ := range cache {
-		ans = append(ans, k)
+	return
+}
+
+func readBinaryWatch1(turnedOn int) (ans []string) {
+	for h := uint8(0); h < 12; h++ {
+		for m := uint8(0); m < 60; m++ {
+			if bits.OnesCount8(h)+bits.OnesCount8(m) == turnedOn {
+				ans = append(ans, fmt.Sprintf("%d:%02d", h, m))
+			}
+		}
 	}
 	return
 }
